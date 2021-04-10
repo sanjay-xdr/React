cadd36aa2e475bbd53aa512deeee61ca18a56d9f
sanjay <sanjayjaiswal586@gmail.com>
2021-04-10 18:47:47 +0530

    getting the location of the user

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,9 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const App=()=>{
-  return <h2>hello there</h2>
-}
+
+
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state={lat:null,errorMSg:''};
+
+    window.navigator.geolocation.getCurrentPosition(
+      (success)=>{ this.setState({lat: success.coords.latitude});
+      
+      
+      
+      
+      }, (error)=>{
+        this.setState({errorMSg: error.message});
+      }
+    );
+
+   
+  }
+  render(){
+
+    
+    
+    if(!this.state.errorMSg && this.state.lat){
+      return <div>Lat:{this.state.lat}</div>
+    }
+    if(this.state.errorMSg && !this.state.lat){
+      return <div>Error:{this.state.errorMSg}</div>
+    }
+    
+    return <div>LOADING</div>
+
+
+ 
+     
+
+  
+
+  };
+};
 
 
 
