c739cf23e6b96be5812a94d4488ac8c0f30c86fb
sanjay <sanjayjaiswal586@gmail.com>
2021-04-10 17:05:32 +0530

    init commit

---
+++ seasons/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App=()=>{
+  return <h2>hello there</h2>
+}
+
+
+
+
+ReactDOM.render(<App/>,document.querySelector("#root"))
