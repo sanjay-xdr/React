cbc7d424ed6b74c16587d9718f0d8db1480460d2
sanjay <sanjayjaiswal586@gmail.com>
2021-04-10 07:34:39 +0530

    making comment component

--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,38 @@
 import react from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 
 const App = ()=>{
-  return <h1>HI there</h1>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatat">
+          <img alt="avatat" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">Sam</a>
+          <div className="metadata">
+            <span className="date">Today at 6:00Pm</span>
+          </div>
+          <div className="text">Nice blog popst</div>
+        </div>
+      </div>
+      {/* i am the comment */}
+      <div className="comment">
+        <a href="/" className="avatat">
+          <img alt="avatat" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">Sam</a>
+          <div className="metadata">
+            <span className="date">Today at 6:00Pm</span>
+          </div>
+          <div className="text">Nice blog popst</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 
