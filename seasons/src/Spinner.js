31e88c51108c623be83cf8dfdd3f0b5e3df8b1d2
sanjay <sanjayjaiswal586@gmail.com>
2021-04-11 10:50:08 +0530

    showing the season by knowing the location

---
+++ seasons/src/Spinner.js
@@ -0,0 +1,22 @@
+import react from 'react';
+
+
+const Spinner=(props)=>{
+
+    return(
+        <div className="ui active dimmer">
+            <div className="ui big text loader">
+                {props.message}
+            </div>
+        </div>
+
+    )
+}
+
+
+Spinner.defaultProps={
+    message:"loading"
+}
+
+
+export default Spinner;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 
@@ -7,36 +9,41 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
 
-    this.state={lat:null,errorMSg:''};
+state={lat:null,errorMSg:''};
 
+  
+
+   
+ 
+
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      (success)=>{ this.setState({lat: success.coords.latitude});
-      
-      
-      
-      
-      }, (error)=>{
-        this.setState({errorMSg: error.message});
+      (success)=> this.setState({lat: success.coords.latitude}),
+       (error)=>{this.setState({errorMSg: error.message});
       }
     );
 
-   
   }
-  render(){
 
+renderContent(){
+  if(!this.state.errorMSg && this.state.lat){
+    return <SeasonDisplay lat={this.state.lat}/>
+  }
+  if(this.state.errorMSg && !this.state.lat){
+    return <div>Error:{this.state.errorMSg}</div>
+  }
+  
+  return <Spinner message="Please Accept the location request"/>
+
+}
+  render(){
+    return(
+      <div>{this.renderContent()}</div>
+    )
     
     
-    if(!this.state.errorMSg && this.state.lat){
-      return <div>Lat:{this.state.lat}</div>
-    }
-    if(this.state.errorMSg && !this.state.lat){
-      return <div>Error:{this.state.errorMSg}</div>
-    }
-    
-    return <div>LOADING</div>
+   
 
 
  
