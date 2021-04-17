18d3eb9310f138cce1bec96472044a9d2ba616b5
sanjay <sanjayjaiswal586@gmail.com>
2021-04-17 09:23:05 +0530

    adding the searchbar

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -9,10 +9,18 @@ class SearchBar extends React.Component{
   state={term:''};
 
 
+  onFormSubmit=(event)=>{
+      event.preventDefault();
+
+      this.props.onSubmit(this.state.term);
+
+  }
+
+
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form  onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                     <label>Image search </label>
                     <input type="text" value={this.state.term} onChange={e=>{this.setState({term:e.target.value})}}/>
